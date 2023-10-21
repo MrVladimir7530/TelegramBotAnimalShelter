db2fbf8573c48e4d498a519730be7e013ed2c905
MrVladimir <vladimir.volkov-2013@yandex.ru>
2023-10-21 12:37:10 +0300

    Создал ветку "dev"

--- src/main/java/com/example/telegrambotanimalshelter/TelegramBotAnimalShelterApplication.java
+++ src/main/java/com/example/telegrambotanimalshelter/TelegramBotAnimalShelterApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TelegramBotAnimalShelterApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TelegramBotAnimalShelterApplication.class,  args);
+        SpringApplication.run(TelegramBotAnimalShelterApplication.class, args);
     }
 
 }
