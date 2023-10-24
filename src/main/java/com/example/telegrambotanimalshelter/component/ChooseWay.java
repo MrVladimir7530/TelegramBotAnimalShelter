036b97d5627397a65da95a2fa3b59ac76c150f25
MrVladimir <vladimir.volkov-2013@yandex.ru>
2023-10-24 15:43:52 +0300

    Начальная конфигурация бота

---
+++ src/main/java/com/example/telegrambotanimalshelter/component/ChooseWay.java
@@ -0,0 +1,13 @@
+package com.example.telegrambotanimalshelter.component;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Setter
+@Component
+public class ChooseWay {
+    private int ChooseWay = 0;
+
+}
---
+++ src/main/java/com/example/telegrambotanimalshelter/service/ChooseAnimal.java
@@ -0,0 +1,11 @@
+package com.example.telegrambotanimalshelter.service;
+
+import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.meta.api.objects.Update;
+@Service
+public class ChooseAnimal {
+
+    public void onUpdateReceived(Update update) {
+        TelegramBot telegramBot;
+    }
+}
