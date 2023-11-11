928cbdeb05c4e1496bf70a449faf50085957bddd
glinka <alexandraglinka@yandex.ru>
2023-11-11 17:47:36 +0300

    controllers failed

--- src/main/java/com/example/telegrambotanimalshelter/model_Service/ShelterService.java
+++ src/main/java/com/example/telegrambotanimalshelter/model_Service/ShelterService.java
@@ -1,4 +1,17 @@
 package com.example.telegrambotanimalshelter.model_Service;
 
+import com.example.telegrambotanimalshelter.model.Shelter;
+
+import java.util.Collection;
+
 public interface ShelterService {
+//    Shelter addShelter(Shelter shelter);
+//
+//    Object getShelterByShelterType(String shelterType); // Приют кошек, Приют собак
+//
+//    Collection<Shelter> getAllShelters();
+//
+//    Shelter updateShelter(String shelterType, Shelter shelter);
+//
+//    void deleteShelter(String shelterType);
 }
