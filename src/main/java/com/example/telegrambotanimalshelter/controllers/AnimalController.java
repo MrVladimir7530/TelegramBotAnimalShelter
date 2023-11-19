d64c0ba07ad6aa699670dc0b2cc112539c8c4434
glinka <alexandraglinka@yandex.ru>
2023-11-19 16:02:14 +0300

    документирование swagger

--- src/main/java/com/example/telegrambotanimalshelter/controllers/AnimalController.java
+++ src/main/java/com/example/telegrambotanimalshelter/controllers/AnimalController.java
@@ -2,6 +2,10 @@ package com.example.telegrambotanimalshelter.controllers;
 
 import com.example.telegrambotanimalshelter.model_services.AnimalService;
 import com.example.telegrambotanimalshelter.models.Animal;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,38 +18,93 @@ public class AnimalController {
         this.animalService = animalService;
     }
 
+    @Operation(
+            summary = "Добавление животного",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Добавленное животное"
+                    )
+            },
+            tags = "Animals"
+    )
     @PostMapping
     public Animal addAnimal(@RequestBody Animal animal) {
         return animalService.addAnimal(animal);
     }
 
+    @Operation(
+            summary = "Возвращает:\n" +
+                    "1) Всех животных (параметры вводить не нужно)\n" +
+                    "2) Животных из конкретного приюта\n" +
+                    "3) Конкретного животного по имени",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Найденные животные"
+                    )
+            },
+            tags = "Animals"
+    )
     @GetMapping
-    public ResponseEntity getAnimals(@RequestParam(required = false) String animalName,
+    public ResponseEntity getAnimals(@Parameter(example = "Приют кошек / Приют собак") @RequestParam(required = false, name = "Тип приюта") String shelterType,
                                      @RequestParam(required = false) String animalType) {
-        if (animalName != null && !animalName.isBlank() && animalType.isBlank()) {
-            return ResponseEntity.ok(animalService.getAnimalByName(animalName));
+        if (shelterType != null && !shelterType.isBlank() && animalType.isBlank()) {
+            return ResponseEntity.ok(animalService.getAnimalByName(shelterType));
         }
-        if (animalType != null && !animalType.isBlank() && animalName.isBlank()) {
+        if (animalType != null && !animalType.isBlank() && shelterType.isBlank()) {
             return ResponseEntity.ok(animalService.getAllAnimalsByType(animalType));
         }
         return ResponseEntity.ok(animalService.getAllAnimals());
     }
 
+    @Operation(
+            summary = "Возвращает:\n" +
+                    "1) Всех усыновленных животных (параметры вводить не нужно)\n" +
+                    "2) Усыновленных животных из конкретного приюта",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Найденные животные"
+                    )
+            },
+            tags = "Animals"
+    )
     @GetMapping("/adopted")
-    public ResponseEntity getAdoptedAnimal(@RequestParam(required = false) String animalType) {
-        if (animalType != null && !animalType.isBlank()) {
-            return ResponseEntity.ok(animalService.getAllAdoptedAnimalsByType(animalType));
+    public ResponseEntity getAdoptedAnimal(@Parameter(example = "Приют кошек / Приют собак") @RequestParam(required = false, name = "Типа приюта") String shelterType) {
+        if (shelterType != null && !shelterType.isBlank()) {
+            return ResponseEntity.ok(animalService.getAllAdoptedAnimalsByType(shelterType));
         }
         return ResponseEntity.ok(animalService.getAllAdoptedAnimals());
     }
 
+    @Operation(
+            summary = "Изменение животного по ID",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Результат правок"
+                    )
+            },
+            tags = "Animals"
+    )
     @PutMapping
-    public Animal updateAnimal(@PathVariable Long id, @RequestBody Animal animal) {
+    public Animal updateAnimal(@PathVariable(name = "ID") Long id, @RequestBody Animal animal) {
         return animalService.updateAnimal(id, animal);
     }
 
+    @Operation(
+            summary = "Удаление животного по ID",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "При успешном удалении код 200"
+                    )
+            },
+            tags = "Animals"
+    )
     @DeleteMapping
-    public void deleteAnimal(@PathVariable Long id) {
+    public void deleteAnimal(@PathVariable(name = "ID") Long id) {
         animalService.deleteAnimal(id);
     }
 }
--- src/main/java/com/example/telegrambotanimalshelter/controllers/ShelterController.java
+++ src/main/java/com/example/telegrambotanimalshelter/controllers/ShelterController.java
@@ -2,6 +2,9 @@ package com.example.telegrambotanimalshelter.controllers;
 
 import com.example.telegrambotanimalshelter.models.Shelter;
 import com.example.telegrambotanimalshelter.model_services.ShelterService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
@@ -16,28 +19,78 @@ public class ShelterController {
         this.shelterService = shelterService;
     }
 
+    @Operation(
+            summary = "Добавление приюта",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Добавленное приюта"
+                    )
+            },
+            tags = "Shelters"
+    )
     @PostMapping
     public Shelter addShelter(@RequestBody Shelter shelter) {
         return shelterService.addShelter(shelter);
     }
 
+    @Operation(
+            summary = "Возвращает конкретных приют",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Найденный приют"
+                    )
+            },
+            tags = "Shelters"
+    )
     @GetMapping
-    public Shelter getShelterByShelterType(@RequestParam String shelterType) {
+    public Shelter getShelterByShelterType(@Parameter(example = "Приют кошек / Приют собак") @RequestParam(name = "Типа приюта") String shelterType) {
         return shelterService.getShelterByShelterType(shelterType);
     }
 
+    @Operation(
+            summary = "Возвращает все приюты",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Найденные приют"
+                    )
+            },
+            tags = "Shelters"
+    )
     @GetMapping("/all")
     public Collection<Shelter> getAllShelters() {
         return shelterService.getAllShelters();
     }
 
+    @Operation(
+            summary = "Изменение приюта по типу",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Результат правок"
+                    )
+            },
+            tags = "Shelters"
+    )
     @PutMapping
-    public Shelter updateShelter(@PathVariable String shelterType, @RequestBody Shelter shelter) {
+    public Shelter updateShelter(@Parameter(example = "Приют кошек / Приют собак") @PathVariable(name = "Тип приюта") String shelterType, @RequestBody Shelter shelter) {
         return shelterService.updateShelter(shelterType, shelter);
     }
 
+    @Operation(
+            summary = "Удаление приюта по ID",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "При успешном удалении код 200"
+                    )
+            },
+            tags = "Shelters"
+    )
     @DeleteMapping
-    public void deleteShelter(@PathVariable String shelterType) {
+    public void deleteShelter(@Parameter(example = "Приют кошек / Приют собак") @PathVariable(name = "Тип приюта") String shelterType) {
         shelterService.deleteShelter(shelterType);
     }
 }
